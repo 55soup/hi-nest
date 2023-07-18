697908a8645f7d43b59a9628d0c85efd84588877
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-07-19 00:37:14 +0900

    service, controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +8,12 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  // controller: url를 가져옴, function 실행
+  // business service: 실제로 function을 가짐.
+  @Get('/hello')
+  sayHello(): string {
+    return this.appService.getHi();
   }
 }
