b33dd928a834a530d839ee434899b75be417391d
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-08-17 22:56:41 +0900

    entity생성, movies 로직 - services

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,31 +1,43 @@
-import { Controller, Get, Param, Post, Delete, Patch, Body, Query} from '@nestjs/common';
+import { 
+    Controller,
+    Get,
+    Param,
+    Post,
+    Delete,
+    Patch,
+    Body,
+    Query} from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { Movie } from './entities/movie.entity';
 
 @Controller('movies')
 export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) {}
+
     @Get()
-    getAll(){
-        return 'This will return all movies';
+    getAll(): Movie[]{
+        return this.moviesService.getAll();
     }
 
-    @Get("search") // ("/:id") 밑에 있어야함.
-    search(@Query("year") searchingYear : string){ // @Query : query string을 가져옴.
-        return `We are searching for a movie with a title : ${searchingYear}`
-    }
+    // @Get("search") // ("/:id") 밑에 있어야함.
+    // search(@Query("year") searchingYear : string){ // @Query : query string을 가져옴.
+    //     return `We are searching for a movie with a title : ${searchingYear}`
+    // }
 
-    @Get("/:id")
-    getOne(@Param("id") movieId: string){
+    @Get(":id")
+    getOne(@Param("id") movieId: string): Movie{
         // @Param: url parameter 가져오기
-        return `This wil return one movie with the id : ${movieId}`;
+        return this.moviesService.getOne(movieId);
     }
 
     @Post()
     create(@Body() movieData){// @Body(): body 데이터 가져오기
-        return movieData;
+        return this.moviesService.create(movieData);
     }
 
     @Delete("/:id")
     remove(@Param("id") movieId: string){
-        return `This will delete a movie with the id : ${movieId}}`;
+        return this.moviesService.deleteOne(movieId);
     }
     
     @Patch("/:id")
