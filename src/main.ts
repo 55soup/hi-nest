83b5966f0fdfb25e9f9c195aff0289b30efe9b3a
55soup <w2118@e-mirim.hs.kr>
2023-09-01 11:29:04 +0900

    movie service first UNIT TEST

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 미들웨어와 비슷함.
   app.useGlobalPipes(new ValidationPipe({
-    whitelist : true,
+    whitelist : true, // 데코레이터가 없는 속성은 제거되어 저장됨.
     forbidNonWhitelisted: true, //기본적으로 필요하지 않는 필드는 무시하지만 클라이언트에게 경고를 반환
     transform: true,
   }));
