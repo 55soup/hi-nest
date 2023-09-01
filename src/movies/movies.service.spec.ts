05e7a1c6baf851bac861c5afff412f6f1343452c
55soup <w2118@e-mirim.hs.kr>
2023-09-01 11:44:52 +0900

    movie.service testing

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
+import { NotFoundException } from "@nestjs/common";
 
 describe('MoviesService', () => {
   let service: MoviesService;
@@ -12,11 +13,46 @@ describe('MoviesService', () => {
     service = module.get<MoviesService>(MoviesService);
   });
 
+  /*
+  it("test name", () => {
+    expect something
+  })
+  */
+
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 
-  it("should be 4", () => {
-    expect(2 + 3).toEqual(5);
+  describe("getAll", () => {
+    it('should return an array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array); // return 타입이 배열인가?
+    })
   })
+
+  describe("getOne", () => {
+    // test를 위한 더미데이터 생성
+    it('should return a movie', () => {
+      service.create({
+        title: "Test Movie",
+        genres: ['test'],
+        year: 2000,
+      });
+
+      const movie = service.getOne(1);
+      expect(movie).toBeDefined();
+      // expect(movie.id).toEqual(1);
+      // expect(movie.title).toEqual("Test Movie");
+    });
+
+    // 에러 test
+    it("should throw 404 error", () => {
+      try{
+        service.getOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        // expect(e.message).toEqual(`Movie with ID: 999 not found`)
+      }
+    })
+  });
 });
