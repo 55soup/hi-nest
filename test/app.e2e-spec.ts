19cfda1291f5193f977512082cab87a82546a9b1
55soup <w2118@e-mirim.hs.kr>
2023-09-02 10:20:51 +0900

    GET, POST, DELETE e2e 테스트

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -19,6 +19,36 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
   });
+
+  describe("/movies", () => {
+
+    it("GET", () => {
+      return request(app.getHttpServer()) // localhost:3000 생략
+        .get("/movies")
+        .expect(200)
+        .expect([]); // 빈 배열 반환
+    })
+
+    it("POST", () => {
+      return request(app.getHttpServer())
+        .post("/movies")
+        .send({
+          title: 'Test',
+          year: 2021,
+          geners: ['test']
+        })
+        .expect(201);
+    })
+
+    it("DELETE", () => {
+      return request(app.getHttpServer())
+        .delete("/movies")
+        .expect(404);
+    })
+
+  });
+  
+
 });
