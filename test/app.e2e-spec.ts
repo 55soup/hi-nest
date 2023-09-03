cabbeb4b01d4403dc715c7448c31608b3e51b881
55soup <w2118@e-mirim.hs.kr>
2023-09-03 12:10:53 +0900

    beforeEach -> beforeAll

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HttpStatus, INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  // beforeAll: Test시 새로운 애플리케이션을 생성x
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
-    app = moduleFixture.createNestApplication(); 
-    // transform 적용
-    app.useGlobalPipes(new ValidationPipe({
-      whitelist : true, // 데코레이터가 없는 속성은 제거되어 저장됨.
-      forbidNonWhitelisted: true, //기본적으로 필요하지 않는 필드는 무시하지만 클라이언트에게 경고를 반환
-      transform: true,
-    }));
+    app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -38,13 +39,13 @@ describe('AppController (e2e)', () => {
         .expect([]); // 빈 배열 반환
     })
 
-    it("POST", () => {
+    it("POST 201", () => {
       return request(app.getHttpServer())
         .post("/movies")
         .send({
           title: 'Test',
-          year: 2021,
-          genres: ['test']
+          year: 2000,
+          genres: ['test'],
         })
         .expect(HttpStatus.CREATED);
     })
@@ -53,17 +54,21 @@ describe('AppController (e2e)', () => {
       return request(app.getHttpServer())
         .delete("/movies")
         .expect(HttpStatus.NOT_FOUND);
+    });
+  });
+
+  describe("/movies/:id", () => {
+    // it.todo("GET")
+    it("GET 200", () => {
+      return request(app.getHttpServer())
+        .get("/movies/1")
+        .expect(HttpStatus.OK);
     })
 
-    describe("/movies/:id", () => {
-      // it.todo("GET")
-      it("GET 200", () => {
-        return request(app.getHttpServer())
-          .get("/movies/1")
-          .expect(HttpStatus.NOT_FOUND);
-      })
-      it.todo("POST")
-      it.todo("DELETE")
+    it("GET 404", () => {
+      return request(app.getHttpServer())
+        .get("/movies/999")
+        .expect(HttpStatus.NOT_FOUND);
     })
   });
 });
